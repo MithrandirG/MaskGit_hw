3b8f905b8259d13d3bd62704bf36986092bec50e
tc_2003 <tc20031027@outlook.com>
2024-01-14 22:39:51 +0800

    change

--- test.py
+++ test.py
@@ -1,21 +1,32 @@
 #from torchsummary import summary
 from torchvision.datasets import MNIST
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader,Dataset
 from torchvision.transforms import Normalize, Compose, ToTensor
 from tqdm import tqdm
 from torch.cuda.amp import autocast
 from torchvision.utils import make_grid
+from torchvision import transforms
 import matplotlib.pyplot as plt
 from models import vqvae
+# from dataset import TinyImageNet
+# from dataset import TrainTinyImageNet, ValTinyImageNet,load_tinyimagenet
+from dataset import TinyImageNet
 import torch
-
+import numpy as np
+import os
+import cv2
+import argparse
 
 vae = vqvae.VQVAE(1, 28, 100, [16, 64, 128]).to('cuda')
 transform = Compose([
     ToTensor(),
     Normalize((0.1307,), (0.3081,))
 ])
-dataloader = DataLoader(MNIST(root='./', download=True, transform=transform), batch_size=512)
+
+# trainset = MNIST(root='./', download=True, transform=transform, train=True)
+data_dir = './tiny-imagenet-200/'
+trainset = TinyImageNet(data_dir, train=True)
+dataloader=DataLoader(trainset, batch_size=512)
 optimizer = torch.optim.AdamW(vae.parameters(), lr=1e-3)
 
 tqdm_bar=tqdm(range(5))
@@ -31,7 +42,9 @@ for ep in tqdm_bar:
         if i%10==0:
             tqdm_bar.set_description('loss: {}'.format(loss['loss']))
 
-dataloader_test = DataLoader(MNIST(root='./', download=True, transform=transform, train=False), batch_size=16, shuffle=True)
+# dataloader_test = DataLoader(MNIST(root='./', download=True, transform=transform, train=False), batch_size=16, shuffle=True)
+testset = TinyImageNet(data_dir, train=False)
+dataloader_test=DataLoader(testset, batch_size=16, shuffle=True)
 # vae.load_state_dict(torch.load('./vae.pt'))
 vae.eval()
 for x,_ in dataloader_test:  
